f23b903c93a567d7d10a524e915fec505432d406
MXPSQL <73059135+MXP2095onetechguy@users.noreply.github.com>
2025-12-14 22:45:56 +0700

    EZier

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from "vite";
 
+import { globSync } from "glob";
+import { resolve } from "node:path";
+
 export default defineConfig({
     base: "./",
-    publicDir: "www",
+    build: {
+        rolldownOptions: {
+            input: Object.fromEntries(
+                globSync('www/**/*.{html,htm,css,js}').map(file => [
+                  // The name of the entry point (e.g., 'main', 'utils/helper')
+                  // This removes the 'src/' part of the path and the file extension
+                  file.slice(4, file.length - 3),
+                  // The absolute path to the file
+                  resolve(process.cwd(), file)
+                ])
+            ),
+            output: {
+              entryFileNames: '[name].[hash].js', // default
+              chunkFileNames: '[name].[hash].js', // default
+              assetFileNames: '[name].[hash].[ext]'
+            }
+        }
+    }
 });
